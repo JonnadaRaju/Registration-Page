1f0ef528d871bfc197e0078d042c158d14055ec9
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-27 07:24:15 +0530

    Add get_db dependency for Route handlers

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from database import create_students_table, get_db_connection
+from fastapi import FastAPI, Depends
+from database import create_students_table, get_db_connection, get_db
 from models import Student
 
 app = FastAPI()
@@ -15,20 +15,16 @@ async def root():
     return {"message": "Welcome to the Student Registration API!"}   
     
 @app.post("/register")
-async def register_student(student: Student):
-    conn = get_db_connection()
-    cursor = conn.cursor()
+async def register_student(student: Student, db=Depends(get_db)):
+    cursor = db.cursor()
     cursor.execute("INSERT INTO students (name, email, phone, course) VALUES (?, ?, ?, ?)", (student.name, student.email, student.phone, student.course))
-    conn.commit()
-    conn.close()
+    db.commit()
     return {"message": "Student registered successfully"}
 
 
 @app.get("/students")
-async def get_students():
-    conn = get_db_connection()
-    cursor = conn.cursor()
+async def get_students(db=Depends(get_db)):
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM students")
     students = cursor.fetchall()
-    conn.close()
     return {"students": [dict(student) for student in students]}
