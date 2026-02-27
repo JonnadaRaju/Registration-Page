74bf619b5ce95dbdd627c196b04afcfb9e7da89e
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-27 07:08:09 +0530

    import students table from database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
+from database import create_students_table
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
     print("Starting up the application...") 
+    create_students_table()
