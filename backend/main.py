89451922454a3000dacac7b7876c99876d3e3a65
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-27 07:12:29 +0530

    Add POST method for register

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from database import create_students_table
+from database import create_students_table, get_db_connection
+from models import Student
 
 app = FastAPI()
 
@@ -7,3 +8,13 @@ app = FastAPI()
 async def startup_event():
     print("Starting up the application...") 
     create_students_table()
+    
+    
+@app.post("/register")
+async def register_student(student: Student):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO students (name, email, phone, course) VALUES (?, ?, ?, ?)", (student.name, student.email, student.phone, student.course))
+    conn.commit()
+    conn.close()
+    return {"message": "Student registered successfully"}
